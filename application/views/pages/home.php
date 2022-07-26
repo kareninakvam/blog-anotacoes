63c39ff875cd7feddaf958d87a9997a7047dd5da
Karen Kvam <karen.kvam@ymail.com>
2022-07-26 18:17:09 -0300

    first commit

---
+++ application/views/pages/home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="http://localhost/projetos/CI/blog-anotacoes/assets/css/style.css">
+	
+    <title>Blog</title>
+</head>
+
+<body>
+        <!---------- Título --------->
+        <img class="title" src="http://localhost/projetos/CI/blog-anotacoes/assets/img/titulo-redimensionado.png" alt="Titulo">
+        
+        <!---------- Barra de navegação --------->
+        <div class="nav-bar-blog">
+            <div class="nav-bar-link">
+                <a href="#">
+                  HOME  
+                </a>
+            </div>
+            <div class="nav-bar-link">
+                <a href="#">
+                  SOBRE MIM   
+                </a>
+            </div>
+            <div class="nav-bar-link">
+                <a href="#">
+                  CONTATO 
+                </a>
+            </div>
+        </div>
+        <hr>
+        <!---------- Corpo do Blog --------->
+        <section>
+            <div class="container">
+                <div class="timeline-posts">
+                    <div class="post">
+                        <h2>Primeiro artigo!</h2>
+                        <div class="date">
+                            21/03/2024
+                        </div>
+                        <div class="post-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies vestibulum tellus, ac ullamcorper justo interdum elementum. Maecenas vitae lectus nec diam elementum condimentum. Pellentesque varius rutrum odio, vitae posuere orci ullamcorper at. Aenean feugiat ante tincidunt, posuere nisl vel, aliquet elit. Etiam id pretium purus, sit amet vulputate diam. Aenean facilisis felis lorem, vitae sollicitudin mi tempus nec. Etiam ultrices blandit mi, at consectetur tellus euismod sed. Cras ac scelerisque neque. Praesent metus elit, tincidunt sed velit vitae, bibendum aliquet risus.
+        
+                            Praesent ac libero ac lectus tempus porttitor. Proin at egestas est, sed mattis lacus. Nulla ornare ex at pretium hendrerit. Vivamus interdum massa ac vestibulum efficitur. Cras convallis orci a erat lobortis, eu vestibulum turpis posuere. Aliquam ullamcorper gravida turpis sed tempor. Vestibulum eget elit erat.
+                        </div>
+                    </div>
+                    <div class="post">
+                        <h2>Segundo artigo!</h2>
+                        <div class="date">
+                            21/03/2024
+                        </div>
+                        <div class="post-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies vestibulum tellus, ac ullamcorper justo interdum elementum. Maecenas vitae lectus nec diam elementum condimentum. Pellentesque varius rutrum odio, vitae posuere orci ullamcorper at. Aenean feugiat ante tincidunt, posuere nisl vel, aliquet elit. Etiam id pretium purus, sit amet vulputate diam. Aenean facilisis felis lorem, vitae sollicitudin mi tempus nec. Etiam ultrices blandit mi, at consectetur tellus euismod sed. Cras ac scelerisque neque. Praesent metus elit, tincidunt sed velit vitae, bibendum aliquet risus.
+        
+                            Praesent ac libero ac lectus tempus porttitor. Proin at egestas est, sed mattis lacus. Nulla ornare ex at pretium hendrerit. Vivamus interdum massa ac vestibulum efficitur. Cras convallis orci a erat lobortis, eu vestibulum turpis posuere. Aliquam ullamcorper gravida turpis sed tempor. Vestibulum eget elit erat.
+                        </div>
+                    </div>
+                </div>
+
+                <!----------Informações adicionais e Welcome --------->
+                <div class="info">
+                    <div class="welcome">
+                        <h3>WELCOME</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies vestibulum tellus, ac ullamcorper justo interdum elementum.</p>
+                    </div>
+                    
+                    <div>
+                        <img class="social-media" src="http://localhost/projetos/CI/blog-anotacoes/assets/img/icones.png" alt="Mídias Sociais">
+                         <!-- A imagem com os ícones é provisória. Posteriormente vou alinhar ícones nesse espaço. -->
+                    </div>           
+                </div>
+            </div>
+        </section>
+</body>
+</html>
